5457047e0022cc9f36979d1d69541f164dffc527
edc37 <grammar.nazi.410@gmail.com>
2023-04-24 14:48:07 -0500

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Magic number: ");
+        string magic = Console.ReadLine();
+        float magicNum = float.Parse(magic);
+
+        Console.Write("Guess: ");
+        string guessString = Console.ReadLine();
+        float guess = float.Parse(guessString);
+
+        if (guess < magicNum) {
+            Console.Write("Higher");
+        }
+        else if (guess > magicNum) {
+            Console.Write("Lower");
+        }
+        else {
+            Console.Write("That's correct!");
+        }
     }
 }

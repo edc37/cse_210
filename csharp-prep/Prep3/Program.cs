b85849e49be4556dad982f794e22b68ee694b12b
edc37 <grammar.nazi.410@gmail.com>
2023-04-24 15:02:53 -0500

    random

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,26 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Magic number: ");
-        string magic = Console.ReadLine();
-        float magicNum = float.Parse(magic);
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,11);
+
+        //Console.Write("Magic number: ");
+        //string magic = Console.ReadLine();
+        //float magicNum = float.Parse(magic);
 
         Console.Write("Guess: ");
         string guessString = Console.ReadLine();
-        float guess = float.Parse(guessString);
+        int guess = int.Parse(guessString);
         
         do {
 
         if (guess < magicNum) {
-            Console.Write("Higher");
+            Console.Write("Higher. ");
         }
         else {
-            Console.Write("Lower");
+            Console.Write("Lower. ");
         }
 
         Console.Write("Guess again: ");
         string guessAgain = Console.ReadLine();
-        guess = float.Parse(guessAgain);
+        guess = int.Parse(guessAgain);
         
         } while(guess != magicNum);
         

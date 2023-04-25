d3fbe682ef0672ca2d5172aa265b55ce3800a96c
edc37 <grammar.nazi.410@gmail.com>
2023-04-24 21:08:51 -0500

    functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        float fave = PromptUserNumber();
+        float square = SquareNumber(fave);
+
+        DisplayResult(name, fave, square);
+    }
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName() {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static float PromptUserNumber() {
+        Console.WriteLine("Please enter your favorite  number: ");
+        string faveString = Console.ReadLine();
+        float fave = float.Parse(faveString);
+        return fave;
+    }
+    static float SquareNumber(float fave) {
+        float square = fave * fave;
+        return square;
+    }
+    static void DisplayResult(string name, float fave, float square) {
+        Console.WriteLine($"{name}, {fave} squared is {square}.");
     }
 }
